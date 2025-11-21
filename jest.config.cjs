b32058c82b1e34e0fc15c3696b9886d35ae27065
Sam-Safari <samuelemanman@gmail.com>
2025-11-21 11:06:56 +0300

    fixed failed tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,8 @@
 module.exports = {
   testEnvironment: 'jsdom',
+  testEnvironmentOptions: {},
   setupFilesAfterEnv: ['<rootDir>/src/__tests__/jest.setup.js'],
+  testMatch: ['**/__tests__/**/*.test.jsx'],
   moduleNameMapper: {
     '\\.(css|less|scss)$': 'identity-obj-proxy',
   },
